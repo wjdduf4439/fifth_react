3bdc18e1e6b508e7d1311dca6359bba402b62fc4
wjdduf4439 <wjdduf4439@gmail.com>
2025-02-05 09:56:46 +0900

    20250205 배포

--- src/layout/UserMenu.js
+++ src/layout/UserMenu.js
@@ -26,7 +26,7 @@ const UserMenu = (props) => {
 		<>
 		<TopMenu />
 		<UnderlineBox />
-	{/* 
+
 		<div style={{ position: 'fixed', bottom: 0, left: 0, width: '100%', height: '5px', background: '#f0f0f0', zIndex: 1000 }}>
 		  <motion.div
 			style={{
@@ -39,7 +39,7 @@ const UserMenu = (props) => {
 			transition={{ duration: 0.1 }}
 		  />
 		</div>
-		 */}
+		
 		</>
 	);
 }
